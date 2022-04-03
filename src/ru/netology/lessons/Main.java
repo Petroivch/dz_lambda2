be5abd34fe4ffd5bfaeb420800e1b6089e91e3a7
Petrovich <petr.tikh03@gmail.com>
2022-04-03 16:52:17 +0300

    Initial commit

---
+++ src/ru/netology/lessons/Main.java
@@ -0,0 +1,18 @@
+package ru.netology.lessons;
+
+public class Main {
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listenerError = System.out::println;
+        Worker worker = new Worker(listener, listenerError);
+        worker.start();
+    }
+}
---
+++ src/ru/netology/lessons/Worker.java
@@ -0,0 +1,21 @@
+package ru.netology.lessons;
+
+public class Worker {
+    private final Main.OnTaskDoneListener callback;
+    private final Main.OnTaskErrorListener callbackError;
+
+    public Worker(Main.OnTaskDoneListener callback, Main.OnTaskErrorListener callbackError) {
+        this.callback = callback;
+        this.callbackError = callbackError;
+    }
+
+    public void start() {
+        for (int i = 1; i < 101; i++) {
+            if (i == 33) {
+                callbackError.onError("Error: task " + i + " is not done");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+}
